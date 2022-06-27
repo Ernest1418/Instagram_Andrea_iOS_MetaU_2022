488aa34a8d61176a631e67c63a5b664e1bd2493d
Andrea Gonzalez <andreagonz@fb.com>
2022-06-27 15:56:34 -0700

    Logout feature

--- Instagram/View Controllers/Home Feed/HomeFeedViewController.m
+++ Instagram/View Controllers/Home Feed/HomeFeedViewController.m
@@ -6,12 +6,26 @@
 //
 
 #import "HomeFeedViewController.h"
+#import <Parse/Parse.h>
+#import "SceneDelegate.h"
+#import "LoginViewController.h"
 
 @interface HomeFeedViewController ()
 
 @end
 
 @implementation HomeFeedViewController
+
+- (IBAction)tapLogoutButton:(id)sender {
+    [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
+        // PFUser.current() will now be nil
+    }];
+    SceneDelegate *sceneDelegate = (SceneDelegate *)[UIApplication sharedApplication].connectedScenes.allObjects.firstObject.delegate;
+
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+    sceneDelegate.window.rootViewController = loginViewController;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
